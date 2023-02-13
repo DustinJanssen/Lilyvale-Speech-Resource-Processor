0471a69c2b499139f1b33386319ea7b05bfe5986
dustinjanssen <dustinjanssen109@gmail.com>
2023-02-13 12:20:58 +0900

    Fix Powerpoint not closing, slide size changing more than once

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Office.Interop.PowerPoint;
 using Ookii.Dialogs.Wpf;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Windows;
 
@@ -43,14 +44,19 @@ namespace Lilyvale_Speech_Resource_Processor
             Microsoft.Office.Interop.PowerPoint.Application pptApplication = new();
             Presentation destPresentation = pptApplication.Presentations.Add(Microsoft.Office.Core.MsoTriState.msoTrue);
 
-            destPresentation.Slides.Add(1, PpSlideLayout.ppLayoutBlank);
+            destPresentation.Slides.Add(1, PpSlideLayout.ppLayoutBlank); //Initialize the slide show to stop index error
+            bool sizeSet = false;
 
             foreach (string sourceFile in sourceFiles)
             {
                 Presentation sourcePresentation = pptApplication.Presentations.Open(sourceFile, Microsoft.Office.Core.MsoTriState.msoFalse, Microsoft.Office.Core.MsoTriState.msoTrue, Microsoft.Office.Core.MsoTriState.msoFalse);
-
-                destPresentation.PageSetup.SlideHeight = sourcePresentation.PageSetup.SlideHeight;
-                destPresentation.PageSetup.SlideWidth = sourcePresentation.PageSetup.SlideWidth;
+                
+                if (!sizeSet) //Fix the size of the merged slide show to the size of the first source presentation
+                {
+                    sizeSet = true;
+                    destPresentation.PageSetup.SlideHeight = sourcePresentation.PageSetup.SlideHeight;
+                    destPresentation.PageSetup.SlideWidth = sourcePresentation.PageSetup.SlideWidth;
+                }
 
                 var slides = sourcePresentation.Slides;
                 foreach (Slide sourceSlide in slides)
@@ -58,18 +64,24 @@ namespace Lilyvale_Speech_Resource_Processor
                     destPresentation.Slides.InsertFromFile(sourceFile, destPresentation.Slides.Count, sourceSlide.SlideNumber, sourceSlide.SlideNumber);
 
                     Slide destSlide = destPresentation.Slides[destPresentation.Slides.Count];
+                   
+                    /*
+                     * The following is an failed attempt to fix the background not copying correctly.
+                     */
                     destSlide.FollowMasterBackground = Microsoft.Office.Core.MsoTriState.msoFalse;
                     destSlide.Background.Fill.ForeColor.RGB = sourceSlide.Background.Fill.ForeColor.RGB;
                     destSlide.Background.Fill.BackColor.RGB = sourceSlide.Background.Fill.BackColor.RGB;
-                    destSlide.ColorScheme = sourceSlide.ColorScheme;
-                    NAR(sourceSlide);
+                    
+                    destSlide.ColorScheme = sourceSlide.ColorScheme; //Force the color scheme to be copied
+                    NAR(sourceSlide); //Release the COM for the individual slide
                 }
 
-                NAR(slides);
-                sourcePresentation?.Close();
-                NAR(sourcePresentation);
+                NAR(slides); //Release the COM for the slides object
+                sourcePresentation?.Close(); //Close the source
+                NAR(sourcePresentation); //Release all COM for the source
             }
 
+            destPresentation.Slides[1].Delete(); //Remove the blank initial slide
             destPresentation.SaveAs(outPath);
 
             GC.Collect();
@@ -79,6 +91,8 @@ namespace Lilyvale_Speech_Resource_Processor
             NAR(destPresentation);
             pptApplication.Quit();
             NAR(pptApplication);
+
+            ForceKill();
         }
 
         private static void NAR(object? o)
@@ -91,6 +105,18 @@ namespace Lilyvale_Speech_Resource_Processor
             finally
             {
                 o = null;
+            }
+        }
+
+        private static void ForceKill()
+        {
+            Process[] powerPointProcesses = Process.GetProcessesByName("POWERPNT");
+            if (powerPointProcesses.Length > 0) 
+            {
+                foreach (Process process in powerPointProcesses) 
+                {
+                    process.Kill();
+                }
             }
         }
     }
